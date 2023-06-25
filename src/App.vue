ce29fc52c7de0685ba3b806365e57e0f4cca02e0
锅巴 <105998455+guoba77@users.noreply.github.com>
2023-06-25 16:29:31 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box">
+    <h1>123</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+  h1 {
+    color: #fff;
+  }
+}
+</style>
